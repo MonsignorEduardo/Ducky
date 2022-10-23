1cfb3a2e9b55b28d6e5f5dc8a75c8fce7bad0c3e
Monsignor <monsignor.eduardo@gmail.com>
2022-10-23 14:11:26 +0200

    💄 ⚰️ Delete unused code and fix imports

--- apps/web-ui/src/server/trpc/router/auth.ts
+++ apps/web-ui/src/server/trpc/router/auth.ts
@@ -1,8 +1,4 @@
-import { Prisma } from '@prisma/client';
-import { TRPCError } from '@trpc/server';
-import { z } from 'zod';
-
-import { protectedProcedure, publicProcedure, router } from '../trpc';
+import { publicProcedure, router } from '../trpc';
 
 export const authRouter = router({
     getSession: publicProcedure.query(({ ctx }) => {
